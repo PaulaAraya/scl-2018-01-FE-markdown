48faef3281b6f887b3c4ec881752d36d557f9128
PaulaAraya <paula.arayawilliams@gmail.com>
2018-08-29 11:08:10 -0300

    funcion comando linea y funcion read file

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 #!/usr/bin/env node
+// requireds
+const fs = require('fs');
+const path = require('path');
+const markdownLinkExtractor = require('./lib/md-links').markdownLinkExtractor;
+const argvYargs = require('./cli').argv;
+
+
+// FUNCION PARA EL COMANDO LINEA
+function commandLine(command) {
+  command = argvYargs._[0];
+
+  switch (command) {
+  case 'receiveFile':
+    receiveFile();
+    console.log('Analizando si su archivo contiene links');
+    break;
+  default:
+    console.log('Comando no es reconocido');
+  }
+};
+
+// funcion que toma el archivo md y lo extrae links
+function readFiles(filemd) {
+  let pathFile = path.join(process.cwd(), filemd);
+  // Ruta actual del archivo
+  let markdownFile = fs.readFileSync(`${pathFile}`, 'uft-8');
+  // Validar .md 
+  if (path.extname(markdownFile).toLowerCase() === '.md') {
+    markdownLinkExtractor(markdownFile).forEach(links => {
+      console.log(links.href, links.text);
+      fetch(`${links.href}`)
+        .then((response) => {
+          console.log(response.url, response.statusText, response.status);
+        });
+    });
+  }
+};
+
+function receiveFile() {
+  let argv = process.argv;
+  let parametro = argv[3];
+  filemd = parametro.split('=')[1];
+  console.log(filemd);
+  readFiles(filemd);
+};
+
+module.exports = {
+  receiveFile: receiveFile,
+  readFiles: readFiles,
+};
+
+/*
+ 
+  // ruta archivo path.join(process.cdw())
+};
+
+console.log(process.cwd());
+
+/* 
+
+
+/*
+ * HACKER EDITION
+
+mdLinks('./some/dir')
+  .then(links => {
+    // => [{ href, text, file }]
+  })
+  .catch(console.error);  */
--- test/md-links.test.js
+++ test/md-links.test.js
@@ -1,5 +1,13 @@
-const mdLinks = require('./../lib/md-links');
 
-describe('', () =>{
+const markdownLinkExtractor = require('./../lib/md-links').markdownLinkExtractor;
 
+test('markdownLinkExtractor debe exisitr una función', () => {
+  expect(markdownLinkExtractor).toBeDefined();
+});
+
+describe('markdownLinkExtractor debe extraer links', function() {
+  test('deberia retornar array vacio si no existe link', function() {
+    const links = markdownLinkExtractor('soy un string  y no un link');
+    expect(links).toEqual([]);
+  });
 });
